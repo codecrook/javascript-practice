477fedff635f8a4998af9aab91b627d3092ad59f
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-02 01:13:11 +0530

    ADD: function to display the punchline

--- random-jokes/app.js
+++ random-jokes/app.js
@@ -1,18 +1,31 @@
 {
-    const [setupDiv, punchlinDiv, punchlineBtn, newJokeBtn, URL] = [
+    const [setupDiv, punchlineDiv, punchlineBtn, newJokeBtn, URL] = [
         document.querySelector('#setup'),
         document.querySelector('#punchline'),
         document.querySelector('#punchlineBtn'),
         document.querySelector('#newJokeBtn'),
         'https://official-joke-api.appspot.com/jokes/programming/random'
     ];
+    let punchline = '';
 
     const getJoke = async (url) => {
         const jokePromise = await fetch(url);
         const joke = await jokePromise.json();
 
-        console.log(joke);
-    }
+        setupDiv.innerHTML = joke[0].setup;
+        punchline = joke[0].punchline;
 
+        punchlineBtn.classList.toggle('hidden');
+        newJokeBtn.classList.toggle('hidden');
+    };
+
+    const getPunchline = () => {
+        punchlineDiv.innerHTML = punchline;
+        punchlineDiv.classList.add('bubble');
+        punchlineBtn.classList.toggle('hidden');
+        newJokeBtn.classList.toggle('hidden');
+    };
+
+    punchlineBtn.addEventListener('click', getPunchline);
     getJoke(URL);
 }
