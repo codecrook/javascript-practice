aee8b92e694079b86d12c3339689335d3d301b87
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-09 01:15:16 +0530

    ADD: render beer names on page

---
+++ beer-app/app.js
@@ -0,0 +1,25 @@
+{
+    const baseURL = "https://api.punkapi.com/v2/beers";
+
+    async function getBeers() {
+        // fetch
+        const beerPromise = await fetch(baseURL);
+        const beers = await beerPromise.json();
+
+        // render data
+        const beersDiv = document.querySelector('.beers');
+
+        let beerHtml = "";
+
+        beers.forEach(beer => {
+            beerHtml += `
+        <h3>${beer.name}</h3>
+       `;
+        });
+
+        beersDiv.innerHTML = beerHtml;
+    }
+
+    // initial get
+    getBeers();
+}
