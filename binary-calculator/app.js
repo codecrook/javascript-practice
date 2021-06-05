2fcbc76ab7f1c07462d5a8f290c87a77fa5c6d60
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-05 16:30:53 +0530

    ADD: evaluate function to evaluate expression

--- binary-calculator/app.js
+++ binary-calculator/app.js
@@ -2,16 +2,25 @@
     const btns = document.querySelector('#btns');
     const res = document.querySelector('#res');
 
-    // function evaluate(){}
+    function evaluate(expression) {
+        const numbers = expression.match(/\d+/g).map(n => parseInt(n, 2)).slice(0, 2);
+        const operator = expression.match(/\+|-|\*|\//)[0];
+        const operations = {
+            "+": (n1, n2) => n1 + n2,
+            "-": (n1, n2) => n1 - n2,
+            "*": (n1, n2) => n1 * n2,
+            "/": (n1, n2) => n1 / n2,
+        };
 
-
+        return operations[operator](...numbers).toString(2);
+    }
     btns.addEventListener('click', e => {
         const btnClicked = e.target.id;
 
         if (btnClicked === 'btnClr') {
             res.innerHTML = '';
         } else if (btnClicked === 'btnEql') {
-            // res.innerHTML = evaluate(textToWrite);
+            res.innerHTML = evaluate(res.innerHTML);
         } else {
             const buttonText = e.target.textContent;
             res.innerHTML += buttonText;
