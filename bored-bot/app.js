0f6de2b8fac410eec4bb23d3cf7c8c915e8edd81
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-21 22:21:07 +0530

    MOD: change backbround color and app title on button click

--- bored-bot/app.js
+++ bored-bot/app.js
@@ -1,12 +1,17 @@
 (function (d) {
     const URL = 'https://apis.scrimba.com/bored/api/activity';
+
+    const appTitle = d.querySelector('#app-title');
     const activityDisplay = d.querySelector('#activity-display');
     const getActivityBtn = d.querySelector('#get-activity');
 
     getActivityBtn.addEventListener('click', () => {
         fetch(URL).then(res => res.json()).then(data => {
             const { activity } = data;
+
+            d.body.classList.add("fun");
             activityDisplay.textContent = activity;
+            appTitle.textContent = "🦾 HappyBot🦿";
         })
     });
 })(document);
